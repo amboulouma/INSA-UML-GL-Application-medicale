9f9a221f700eb64ef8f4d403fc18a359cacf3ef3
aminembouloum <amine@boulouma.com>
2018-05-15 09:41:16 +0200

    Added AttributDouble.h

--- ApplicationMedical/ApplicationMedical/AttributDouble.h
+++ ApplicationMedical/ApplicationMedical/AttributDouble.h
@@ -1,1 +1,18 @@
-#pragma once
+// AttributDouble.h
+#include <string>
+#include "Attribut.h"
+
+#ifndef AttributDouble_H
+#define AttributDouble_H
+
+using namespace std;
+
+class AttributDouble : public Attribut
+{
+private:
+	double data;
+public:
+	double distance(AttributDouble attributDouble);
+};
+
+#endif
