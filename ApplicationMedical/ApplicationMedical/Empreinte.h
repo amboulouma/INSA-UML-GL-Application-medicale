e62174227156b7fb7724548596ad6f8d54780b43
Cao Xuân Huy <caoxuan_huy@hotmail.com>
2018-05-15 10:06:57 +0200

    aaa

--- ApplicationMedical/ApplicationMedical/Empreinte.h
+++ ApplicationMedical/ApplicationMedical/Empreinte.h
@@ -1,1 +1,25 @@
 #pragma once
+// Empreinte.h
+#include <string>
+#include <list>
+#include "Attribut.h"
+#ifndef Empreinte_H
+#define Empreinte_H
+
+using namespace std;
+
+class Empreinte
+{
+	protected:
+		int NoID;
+		list<Attribut*> listeAttributs;
+		static const string FICHIER_EMPREINTES;
+		static const string FICHIER_METADONNEES;
+		static list<int> modele;
+
+	public:
+		double Distance(Empreinte e);
+		void SauvegarderEmpreinte();
+};
+
+#endif
