3e8d4868ed490457781211612f2a9168fda23c9f
Cao Xuân Huy <caoxuan_huy@hotmail.com>
2018-05-29 15:20:55 +0200

    double

--- ApplicationMedical/ApplicationMedical/GestionDonnees.cpp
+++ ApplicationMedical/ApplicationMedical/GestionDonnees.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 GestionDonnees::GestionDonnees() {}
 
-GestionDonnees::GestionDonnees(list<Maladie> listMaladie)
-{
-}
-
 GestionDonnees::~GestionDonnees() {}
 
 void GestionDonnees::analyse(Empreinte e)
@@ -21,9 +17,5 @@ void GestionDonnees::analyse(list<Empreinte> listeEmpreintes)
 }
 
 void GestionDonnees::associerMaladieEmpreinte(string maladie, Empreinte e)
-{
-}
-
-Empreinte GestionDonnees::trouverEmpreinteParID(int id)
 {
 }
